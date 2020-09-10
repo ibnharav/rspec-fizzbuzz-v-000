88403e08a3d91ec2ffb7bd6a0edfa60b9f5a048b
Jonathan Bernstein <jjbernstein@gmail.com>
2020-09-10 23:42:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,15 @@
 def fizzbuzz(int)
   if int % 15 == 0
     return "FizzBuzz"
- else if int % 5 == 0
+  end
+  if int % 5 == 0
    return "Buzz"
- 
- else if int % 3 == 0
+ end
+  if int % 3 == 0
      return "Fizz"
-  else
+  end
      nil
-  end  
+    
 
 
 end
